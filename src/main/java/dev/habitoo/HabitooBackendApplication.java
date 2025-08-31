7f4e434f9a3f34307c0f98b5e830ff5ba88cd8e7
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-31 17:38:03 -0300

    chore(backend): projeto Spring Boot inicial (Gradle) + config Postgres.

---
+++ src/main/java/dev/habitoo/HabitooBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.habitoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitooBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/habitoo/HabitooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.habitoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
